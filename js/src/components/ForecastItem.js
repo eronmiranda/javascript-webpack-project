ae97ce3f9b7b49071b093039070729c15e1e221b
Dan Gilleland <dgilleland@users.noreply.github.com>
2020-12-17 16:54:33 -0700

    Initial commit

---
+++ js/src/components/ForecastItem.js
@@ -0,0 +1,15 @@
+const ForecastItem = (props) => {
+  let {key, forecastDay} = props
+
+  return <li key={key}>
+    <div className="forecast-detail">
+      <p><strong><span className="time">Time: {forecastDay.date}</span></strong></p>
+      <ul>
+        <li className="condition"> Condition: {forecastDay.condition}</li>
+        <li className="temp">Temp: {((forecastDay.high + forecastDay.low) / 2)}</li>
+      </ul>
+    </div>
+  </li>
+}
+
+export default ForecastItem;
